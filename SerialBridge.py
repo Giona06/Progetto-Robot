c74d0b36994bb988a193afafdd6d0c571d18038e
Giona06 <pataissi@libero.it>
2025-04-12 02:02:06 +0200

    Aggiunto metodo per l'unpack dei dati seriali, aggiornata ricezione dati di conseguenza (per ora solo ultrasonico e linefollower)

--- SerialBridge.py
+++ SerialBridge.py
@@ -4,6 +4,15 @@ import threading
 import time
 import requests
 from requests.auth import HTTPBasicAuth
+import struct
+
+#Costanti Server
+PHP_SERVER_URL = 'http://localhost/capolavoro/ricezione.php'
+SOCKET_HOST = 'localhost'
+SOCKET_PORT = 12345
+USERNAME = 'root'
+PASSWORD = 'root'
+
 
 def porteSeriali():
     """Restituisce tutte le porte seriali disponibili"""
@@ -49,13 +58,6 @@ def selezionaBaudrate():
         except ValueError:
             print("Inserisci un numero valido.")
 
-# Costanti
-PHP_SERVER_URL = 'http://localhost/capolavoro/ricezione.php'
-SOCKET_HOST = 'localhost'
-SOCKET_PORT = 12345
-USERNAME = 'root'
-PASSWORD = 'root'
-
 porta = selezionaPortaSeriale()
 baudrate = selezionaBaudrate()
 
@@ -63,15 +65,71 @@ print(f"Porta selezionata: {porta} - Baudrate: {baudrate}")
 
 ser = serial.Serial(porta, baudrate, timeout=1)
 
+#Struttura pacchetto [START_BYTE(1), HEADER_FOLLOW_LINE(1), DATA_FOLLOWLINE(1), HEADER_ULTRASONIC(1), DATA_ULTRASONIC(4), CHECKSUM(1), END_BYTE(1)]
+START_BYTE = b'\xaa'
+HEADER_FOLLOW_LINE = b'\x01'
+HEADER_ULTRASONIC = b'\x02'
+END_BYTE = b'\xff'
+
+LENGTH_FOLLOW_LINE = 1
+LENGTH_ULTRASONIC = 4
+PACKET_SIZE = 10
+
+# Più efficciente usare i byte invece di stringhe :'D
+def unpack():
+    """Unpack dei dati dalla seriale"""
+    while True:
+        if ser.read(1) == START_BYTE:
+            # Sezione follow line
+            header_fl = ser.read(1)
+            if header_fl != HEADER_FOLLOW_LINE:
+                print("Pacchetto non valido")
+                continue
+            data_fl = ser.read(LENGTH_FOLLOW_LINE)
+
+            # Sezione ultrasonico
+            header_us = ser.read(1)
+            if header_us != HEADER_ULTRASONIC:
+                print("Pacchetto non valido")
+                continue
+            data_us = ser.read(LENGTH_ULTRASONIC)
+
+            # Controllo checksum xor
+            checksum = ser.read(1)
+            end = ser.read(1)
+            if end != END_BYTE:
+                print("Pacchetto non valido")
+                continue
+
+            checksum_c = header_fl[0] ^ data_fl[0] ^ header_us[0]
+            for byte in data_us:
+                checksum_c ^= byte
+            checksum_c = bytes([checksum_c])
+
+            if checksum_c != checksum:
+                print("Checksum non valido")
+                continue
+            
+            valore_fl = struct.unpack('<b', data_fl)[0]
+            valore_us = struct.unpack('<f', data_us)[0]
+
+            return {
+                "FollowLine": valore_fl,
+                "Ultrasonic": valore_us
+            }
+            
+    
+
+
 def serialListener():
     """Legge i dati dalla seriale e li invia al server PHP"""
     while True:
         try:
-            if ser.in_waiting:
-                data = ser.readline().decode(errors='ignore').strip()
+            if ser.in_waiting >= PACKET_SIZE:
+                data = unpack()
                 if data:
                     print(f"[SERIALE] → {data}")
-                    response = requests.post(PHP_SERVER_URL, data={'dato': data}, auth=HTTPBasicAuth(USERNAME, PASSWORD))
+                    response = requests.post(PHP_SERVER_URL, data=data, auth=HTTPBasicAuth(USERNAME, PASSWORD))
                     print(f"[PHP] ← {response.status_code}")
         except Exception as e:
             print(f"[ERRORE POST] {e}")
@@ -98,7 +156,7 @@ threading.Thread(target=socketListener, daemon=True).start()
 print("[SERVIZIO] Bridge attivo")
 try:
     while True:
-        time.sleep(0.1) # Da settare tutti i ritardi
+        time.sleep(0.1)
 except KeyboardInterrupt:
     print("\n[SERVIZIO] Interrotto")
     exit(0)
--- web application/ricezione.php
+++ web application/ricezione.php
@@ -11,7 +11,8 @@ if (!isset($_SERVER['PHP_AUTH_USER']) ||
 }
 
 $data = [
-    'temp' => (int) $_POST['temp'] ?? null,
+    'FollowLine' => (int) $_POST['FollowLine'] ?? null,
+    'Ultrasonic' => (float) $_POST['Ultrasonic'] ?? null,
 ];
 file_put_contents('data.json', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
 header('Content-Type: application/json; charset=utf-8');
